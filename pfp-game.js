44e0e514bcdeb7de738b294990ab7ea26e3a24b5
Shalin Chang <shalin.chang8633@gmail.com>
2026-05-04 20:18:56 -0400

    Add files via upload

--- pfp-game.js
+++ pfp-game.js
@@ -1,23 +1,50 @@
   // Get the element
 const pfp = document.getElementById('pfp');
+const headerBg = document.getElementById('header');
+const headerLinks = document.getElementById('header-links');
 let isMouseOnPfp = false;
+let isMouseOnHeaderBg = false;
+let isMouseOnHeaderLinks = false;
 let filterVal = 0;
-
+let angle = 45;
   // Trigger JavaScript when mouse enters
 pfp.addEventListener('mouseenter', () => {
 	isMouseOnPfp = true;
     console.log('Mouse entered!');
     
-  });
+});
 
   // Trigger JavaScript when mouse leaves
-  pfp.addEventListener('mouseleave', () => {
+pfp.addEventListener('mouseleave', () => {
     console.log('Mouse left!');
 	isMouseOnPfp = false;
-  });
+});
+
+headerBg.addEventListener('mouseenter', () => {
+	isMouseOnHeaderBg = true;
+    console.log('Mouse entered header!');
+    
+});
+
+  // Trigger JavaScript when mouse leaves
+headerBg.addEventListener('mouseleave', () => {
+    console.log('Mouse left header!');
+	isMouseOnHeaderBg = false;
+});
+headerLinks.addEventListener('mouseenter', () => {
+	isMouseOnHeaderLinks = true;
+    console.log('Mouse entered header link!');
+    
+});
+
+  // Trigger JavaScript when mouse leaves
+headerLinks.addEventListener('mouseleave', () => {
+    console.log('Mouse left header link!');
+	isMouseOnHeaderLinks = false;
+});
+  
   
 var ready = false;
-
 
 
 
@@ -36,6 +63,17 @@ var mainloop = setInterval(function() {
 	} else {
 		pfp.style.filter = "hue-rotate(" + 0 + 'deg)';
 		filterVal = 0;
+		
+	}
+	if (isMouseOnHeaderBg && !isMouseOnPfp && !isMouseOnHeaderLinks) {
+		headerBg.style.background = "repeating-linear-gradient(" + angle + "deg,#60bc6d,#60bc6d 20px,#469852 20px,#469852 40px)";
+
+		angle += 2;
+		if (angle >= 405) {
+			angle = 45;
+		}
+	} else {
+		//headerBg.style.background = "repeating-linear-gradient(" + 45 + "deg,#60bc6d,#60bc6d 20px,#469852 20px,#469852 40px)";
 	}
 	
 	if (ready) {
